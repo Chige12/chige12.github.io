84d0f0322e5ed895acdb743509e27996bc42bf2b
chige <tige1215.technic@gmail.com>
2017-05-13 22:00:08 +0900

    +UserAgentDiscrimination

--- script/main.js
+++ script/main.js
@@ -8,3 +8,45 @@ $(function(){
         $("#global_nav").removeClass("motion");
     });
 });
+
+$(function () {
+ var ua = navigator.userAgent;
+ if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+     // スマートフォン用コード
+ } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+     // タブレット用コード
+ } else {
+     // PC用コード
+         $.scrollify({
+           section : ".container",
+           sectionName : "section-name",
+           easing: "easeOutExpo",
+           scrollSpeed: 600,
+           offset : 0,
+           scrollbars: true,
+           before:function() {},
+           after:function() {}
+         });
+      }
+  })
+
+  $(".scroll-to-header").click(function(e) {
+  e.preventDefault();
+  $.scrollify.move("#header");
+  });
+  $(".scroll-to-about").click(function(e) {
+  e.preventDefault();
+  $.scrollify.move("#about");
+  });
+  $(".scroll-to-history").click(function(e) {
+  e.preventDefault();
+  $.scrollify.move("#history");
+  });
+  $(".scroll-to-works").click(function(e) {
+  e.preventDefault();
+  $.scrollify.move("#works");
+  });
+  $(".scroll-to-contact").click(function(e) {
+  e.preventDefault();
+  $.scrollify.move("#contact");
+  });
