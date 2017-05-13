7f8e8a47995fbe822bb297375e1730b7f3633ae2
chige <tige1215.technic@gmail.com>
2017-05-13 22:37:14 +0900

    js-test2

--- script/main.js
+++ script/main.js
@@ -13,12 +13,8 @@ $(function () {
  var ua = navigator.userAgent;
  if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
      // スマートフォン用コード
-         $.scrollify.disable();
-         });
  } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
      // タブレット用コード
-         $.scrollify.disable();
-         });
  } else {
      // PC用コード
          $.scrollify({
@@ -32,7 +28,7 @@ $(function () {
            after:function() {}
          });
       }
-  })
+  });
 
   $(".scroll-to-header").click(function(e) {
   e.preventDefault();
