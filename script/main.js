dc9a9610de48563938baf4f6c9f3910ac51eb6a9
chige <tige1215.technic@gmail.com>
2017-05-16 12:46:41 +0900

    works+phone_js

--- script/main.js
+++ script/main.js
@@ -1,55 +1,77 @@
-$(function(){
-    $('#global_nav').hover(function(){
-        $(this).addClass('motion');
-    },function(){
-        $(this).removeClass('motion');
-    });
-    $('.global_nav_item').click(function(){
-        $("#global_nav").removeClass("motion");
-    });
-});
-
-$(function () {
-  $.scrollify({
-    section : ".container",
-    sectionName : "section-name",
-    easing: "easeOutExpo",
-    scrollSpeed: 600,
-    offset : 0,
-    scrollbars: true,
-    before:function() {},
-    after:function() {}
-  });
-
  var ua = navigator.userAgent;
-   if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
-       // スマートフォン用コード
-       $.scrollify.disable();
-   } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
-       // タブレット用コード
-       $.scrollify.disable();
+   if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+       // スマートフォン/タブレット用コード
+       $(function(){
+           // スクロールの速度
+           var speed = 600; // ミリ秒
+          //ページ内スクロール
+          $(".scroll-to-header").click(function () {
+              var i = $(".scroll-to-header").index(this)
+              var p = $("#header").eq(i).offset().top;
+              $('html,body').animate({ scrollTop: p }, speed, 'easeOutExpo');
+              return false;
+          });
+          $(".scroll-to-about").click(function () {
+              var i = $(".scroll-to-about").index(this)
+              var p = $("#about").eq(i).offset().top;
+              $('html,body').animate({ scrollTop: p }, speed, 'easeOutExpo');
+              return false;
+          });
+          $(".scroll-to-works").click(function () {
+              var i = $(".scroll-to-works").index(this)
+              var p = $("#works").eq(i).offset().top;
+              $('html,body').animate({ scrollTop: p }, speed, 'easeOutExpo');
+              return false;
+          });
+          $(".scroll-to-contact").click(function () {
+              var i = $(".scroll-to-contact").index(this)
+              var p = $("#contact").eq(i).offset().top;
+              $('html,body').animate({ scrollTop: p }, speed, 'easeOutExpo');
+              return false;
+          });
+       });
+
    } else {
        // PC用コード
-   }
+       $(function(){
+           $('#global_nav').hover(function(){
+               $(this).addClass('motion');
+           },function(){
+               $(this).removeClass('motion');
+           });
+           $('.global_nav_item').click(function(){
+               $("#global_nav").removeClass("motion");
+           });
+       });
+     $.scrollify({
+       section : ".container",
+       sectionName : "section-name",
+       easing: "easeOutExpo",
+       scrollSpeed: 600,
+       offset : 0,
+       scrollbars: true,
+       before:function() {},
+       after:function() {}
+     });
 
-  $(".scroll-to-header").click(function(e) {
-  e.preventDefault();
-  $.scrollify.move("#header");
-  });
-  $(".scroll-to-about").click(function(e) {
-  e.preventDefault();
-  $.scrollify.move("#about");
-  });
-  $(".scroll-to-history").click(function(e) {
-  e.preventDefault();
-  $.scrollify.move("#history");
-  });
-  $(".scroll-to-works").click(function(e) {
-  e.preventDefault();
-  $.scrollify.move("#works");
-  });
-  $(".scroll-to-contact").click(function(e) {
-  e.preventDefault();
-  $.scrollify.move("#contact");
-  });
-});
+      $(".scroll-to-header").click(function(e) {
+      e.preventDefault();
+      $.scrollify.move("#header");
+      });
+      $(".scroll-to-about").click(function(e) {
+      e.preventDefault();
+      $.scrollify.move("#about");
+      });
+      $(".scroll-to-history").click(function(e) {
+      e.preventDefault();
+      $.scrollify.move("#history");
+      });
+      $(".scroll-to-works").click(function(e) {
+      e.preventDefault();
+      $.scrollify.move("#works");
+      });
+      $(".scroll-to-contact").click(function(e) {
+      e.preventDefault();
+      $.scrollify.move("#contact");
+      });
+}
